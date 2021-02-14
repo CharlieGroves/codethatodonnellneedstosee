32320fbac682a17b4ea35d7a1e9df5be1d704a0c
CharlieGroves <ctrg04@gmail.com>
2021-02-14 15:47:59 +0000

    comments to game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -127,31 +127,66 @@ function Leaderboard(props) {
     END OF submitLeaderboard FUNCTION
     */
 
-
+    /*
+    This returns the built leaderboard in JSX (which is almost HTML apart from a few small
+    things such as inline syles and embedded scripts are different)
+    */
     return (
+        // This div is the container for the whole leaderboard
         <div style={{ minWidth: '97vw', marginRight: '2vw', marginLeft: '1vw'}}>
-                <Form onSubmit={submitLeaderboard}>
-                    <div>
-                        <Form.Group id='text'>
-                        </Form.Group>
+            {/*
+            This form calls the submitLeaderboard function which I explained earlier after
+            the button with type submit is pressed. You'll notice that to comment in JSX, I
+            have to wrap the comment definers as well as the comment itself in curley brackets
+            to signify that is is javascript. This is because JSX doesn't have a built in 
+            comment functiion
+            */}
+            <Form onSubmit={submitLeaderboard}>
+                {/* Container for the button*/}
+                <div>
+                    {/* 
+                    Bootstrap button which acts as the submit button for the form 
+                    it is wrapped in. The "disabled" attribute is where the useState
+                    hook is read. If the disabled variable is set to true, the load
+                    leaderboard button is disabled.
+                    */}
                     <Button disabled={disabled} type="submit">Load Leaderboard</Button>
-                    </div>
-                </Form>
-                <Table responsive='sm' variant='dark'>
-                    <thead>
-                        <th>Username</th>
-                        <th>Score</th>
-                    </thead>
-                    <tbody>
-                {leaderboard && leaderboard.map(foo => 
-                    <tr key={foo.id}>
-                        <td>{foo.text}</td>
-                        <td>{foo.score}</td>
-                    </tr>
-                )}
-                    </tbody>
-                </Table>
+                </div>
+            </Form>
+            
+            {/* END OF FORM */}
+
+            {/* 
+            The leaderboard is wrapped in the Bootstrap Table tags.
+            I marked it as responsive so it is mobile freinfly and dark 
+            for style 
+            */}
+            <Table responsive='sm' variant='dark'>
+                {/* This defines the "Table Headers" */}
+                <thead>
+                    <th>Username</th>
+                    <th>Score</th>
+                </thead>
+                {/* Start of the body of the table */}
+                <tbody>
+                    {/* 
+                    I use the map function built into javascript to assign each
+                    object in the leaderboard object it's own "table row" with the 
+                    key as the id (which is built into firestore), and the text as the name
+                    and score as score. foo is just a placeholder variable for me to assign
+                    stuff too. 
+                    */}
+                    {leaderboard && leaderboard.map(foo => 
+                        <tr key={foo.id}>
+                            <td>{foo.text}</td>
+                            <td>{foo.score}</td>
+                        </tr>
+                    )} {/* END OF MAPPING LEADERBOARD OBJECT */}
+                </tbody>
+            </Table>
+            {/* END OF TABLE */}
        </div>
+       /* END OF LEADERBOARD */
     )
 }
 export default function Game() {
@@ -159,26 +194,42 @@ export default function Game() {
     const [playerOneUsername, setPlayerOneUsername] = useState('Player 1');
     const [playerTwoUsername, setPlayerTwoUsername] = useState('Player 2');
 
+
+    // Defines an empty array which will be eventually filled with cards
     let cards = [];
 
+    // Defines the player's decks which will be eventually filled with cards
     let p1 = [];
     let p2 = [];
 
+
+    // A for look which starts at 1 and ends at 3 for the 3 different possible colours
     for(var i = 1; i < 4; i++) {
+        // A nested for loop which starts at 1 and goes to 10 for each card number in the deck
         for (var j = 1; j < 11; j++) {
+            // If the outside loop is 1, the card colour is red and the card number is the value of j.
+            // cards.push() is the equivilent of cards.append() just in javascript rather than python.
             if (i === 1) {
                 cards.push(`red ${j}`)
+            // Else if the outside loop is 2, the card colour is black and the card number is the value of j.
             } else if (i === 2) {
                 cards.push(`black ${j}`)
+            // Else, the card colour is yellow and the card number is the value of j.
             } else if (i === 3) {
                 cards.push(`yellow ${j}`)
             }
         }
+        // END OF NESTED FOR LOOP
     }
+    // END OF FOR LOOP
 
+    // Defines a new variable 'newCards' which is the cards array we just defined, and now are shuffling using the 
+    // shuffling algorithm previously commented.
     let newCards = shuffle(cards);
 
+    // This for loop alternates between player 1's deck and player 2's deck, assigning the shuffled cards.
     for (var k = 0; k < 30; k++) {
+        // Simple use of modulus to determine whether to push to player 1's deck or player 2's.
         if (k%2 === 0) {
             p1.push(newCards[k])
         } else {
@@ -186,18 +237,24 @@ export default function Game() {
         }
     }
 
+    // Defines 2 variables (p1Score & p2Score) as integers starting at zero.
     let p1Score = 0;
     let p2Score = 0;
 
+    // This algorithm determines who gets points
+    // for every card
     for(let x = 0; x < 15; x++) {
+        // extract the colour of each card per person
         let p1Colour = p1[x].slice(0, -2);
         let p2Colour = p2[x].slice(0, -2);
+        // if the colours are the same, check who had the biggest number and give points to them
         if (p1[x].slice(0, -2) === p2[x].slice(0, -2) || p1[x].slice(0, -3) === p2[x].slice(0, -3)) {
             if(Number(p1[x].substr(p1[x].length - 2)) > Number(p2[x].substr(p2[x].length - 2))) {
                 p1Score++;
             } else {
                 p2Score++;
             }
+        // else, check the colours and award points
         } else {
             if (p1Colour === 'red' && p2Colour === 'black') {
                 p1Score++;
@@ -210,6 +267,7 @@ export default function Game() {
             }
         }
     }
+    // END OF POINTS ALGORITHM
 
     function setPlayerOneUsernameFunction(e) {
         e.preventDefault();
@@ -225,64 +283,107 @@ export default function Game() {
         setPlayerTwoUsername(document.getElementById('playertwo').value)
     }
 
+
+    // This is the main return for the Game function and is called as soon as the /game route is loaded
     return (
+        // This is a JSX element called a fragment which I used here because you can only return one thing
+        // from a function so therefore have to wrap two divs in one main container. The fragment exists for this reason.
         <>
+            {/* This div makes sure that the cards change shape dynamically */}
             <div style={{ marginLeft: "10px", marginTop: '10px', display: 'flex', flexWrap: 'wrap'}}>
+                {/* Wrapping div for flexbox */}
                 <div>
+                        {/* Wrapping paragraph for player one information */}
                         <p> 
+
                             <Form style={{ display: 'flex', flexWrap: 'wrap'}} onSubmit={setPlayerOneUsernameFunction}>
                                 <Form.Control id='playerone' style={{ maxWidth: "75vw", marginRight: '5px' }} placeholder='Player One Username' />
                                 <Button type="submit">Set</Button>
                             </Form>
+                            {/* This is how you put javascript into JSX. I am embedding the Username & the score. */}
                             <h1 id='p1card'>{playerOneUsername}'s Cards</h1>
                             <h2 id='p1points'>{playerOneUsername}'s Points Total: {p1Score}</h2>
                         </p>
+                        {/* END OF PLAYER ONE INFORMATION */}
+                    {/* Another flexbox container that generates all of player one's cards as CardContainer. A Card Container is a custom
+                    HTML tag which I have made. You can do this with React and this is one of the reasons I chose it. */}
                     <div style={{ marginLeft: "10px", marginTop: '10px', display: 'flex', flexWrap: 'wrap'}}>
+                        {/* Each 'card' gets mapped as well as the information being passed through to the CardContainer in the props object */}
                         {p1 && p1.map(card => <CardContainer key={card} card={card} />)}
                     </div>
                 </div>
-                    <div>
-                        <p>
-                            <Form style={{ display: 'flex', flexWrap: 'wrap'}} onSubmit={setPlayerTwoUsernameFunction}>
-                                <Form.Control id='playertwo' style={{ maxWidth: "75vw", marginRight: '5px' }} placeholder='Player Two Username' />
-                                <Button type="submit">Set</Button>
-                            </Form>
-                            <h1 id='p2card'>{playerTwoUsername}'s Cards</h1>
-                            <h2 id='p2points'>{playerTwoUsername}2's Points Total: {p2Score}</h2>
-                        </p>
+                {/* Wrapping div for flexbox */}
+                <div>
+                    {/* Wrapping paragraph for player two information */}
+                    <p>
+                        <Form style={{ display: 'flex', flexWrap: 'wrap'}} onSubmit={setPlayerTwoUsernameFunction}>
+                            <Form.Control id='playertwo' style={{ maxWidth: "75vw", marginRight: '5px' }} placeholder='Player Two Username' />
+                            <Button type="submit">Set</Button>
+                        </Form>
+                        {/* Username & score embedded */}
+                        <h1 id='p2card'>{playerTwoUsername}'s Cards</h1>
+                        <h2 id='p2points'>{playerTwoUsername}2's Points Total: {p2Score}</h2>
+                    </p>
+                    {/* The CardContainer tag as explained above */}
                     <div style={{ marginLeft: "10px", marginTop: '10px', display: 'flex', flexWrap: 'wrap'}}>
                         {p2 && p2.map(card => <CardContainer key={card} card={card} />)}
                     </div>
                 </div>
             </div>
+            {/* END OF PLAYER INFORMATION */}
+
+            {/* The custom Leaderboard HTML tag which I wrote at the start of this file. It passes in player one's score
+            as well as player two's and this is added to the props object which the Leaderboard function can access. */}
                 <Leaderboard p1Score={p1Score} p2Score={p2Score}/>
         </>
     )
 }
 
+// This is the CardContainer function/HTML tag which is used above. It takes in 'prop's which is an object that contains all of the 
+// information that the CardContainer needs to know.
 function CardContainer(props) {
+
+    // I am dynamically applying styles to each of the Cards.
     
+    // If the card's name has the word 'red' in it, then colour it red. This is done with the string manipulation properties of .slice().
     if ("red" === props.card.slice(0, -2) || "red" === props.card.slice(0, -3) ) {
         return(
+            /* The Card tag is a part of react-bootstrap and applies some basic CSS styles. I also added some of my own in terms of margin,
+            max width, min height, the colour of the card and margin on the left & bottom. This makes the cards look like cards rather than
+             boxes */
             <Card style={{ minWidth: '150px', minHeight: '200px', backgroundColor: 'red', marginLeft: '10px', marginBottom: '10px'}}>
+                {/* The Card.Body property has the actual text content of the card. I use string manipulation with the .substr() method which 
+                can extract properties of the string. I am taking the last 2 characters and putting them as the card body as they contain the card number. */}
                 <Card.Body style={{fontSize: "xx-large"}}>{props.card.substr(props.card.length - 2)}</Card.Body>
             </Card>
         )
     }
+    // If the card's name has the word 'black' in it, then colour it black.
     if ("black" === props.card.slice(0, -2) || "black" === props.card.slice(0, -3) ) {
         return(
+            /* The Card tag is a part of react-bootstrap and applies some basic CSS styles. I also added some of my own in terms of margin,
+            max width, min height, the colour of the card and margin on the left & bottom. This makes the cards look like cards rather than
+             boxes */
             <Card style={{ minWidth: '150px', minHeight: '200px', backgroundColor: '#444c54', color: 'white', marginLeft: '10px', marginBottom: '10px'}}>
+                {/* The Card.Body property has the actual text content of the card. I use string manipulation with the .substr() method which 
+                can extract properties of the string. I am taking the last 2 characters and putting them as the card body as they contain the card number. */}
                 <Card.Body style={{fontSize: "xx-large"}}>{props.card.substr(props.card.length - 2)}</Card.Body>
             </Card>
         )
     }
+    // If the card's name has the word 'yellow' in it, then colour it yellow.
     if ("yellow" === props.card.slice(0, -2) || "yellow" === props.card.slice(0, -3) ) {
         return(
+            /* The Card tag is a part of react-bootstrap and applies some basic CSS styles. I also added some of my own in terms of margin,
+            max width, min height, the colour of the card and margin on the left & bottom. This makes the cards look like cards rather than
+             boxes */
             <Card style={{ minWidth: '150px', minHeight: '200px', backgroundColor: 'yellow', marginLeft: '10px', marginBottom: '10px'}}>
+                {/* The Card.Body property has the actual text content of the card. I use string manipulation with the .substr() method which 
+                can extract properties of the string. I am taking the last 2 characters and putting them as the card body as they contain the card number. */}
                 <Card.Body style={{fontSize: "xx-large"}}>{props.card.substr(props.card.length - 2)}</Card.Body>
             </Card>
         )
     }
 }
 
-
+// END OF CARDCONTAINER FUNCTION AND END OF FILE
